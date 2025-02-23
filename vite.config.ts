c6e54316ac0223cf7f8d463d5ffbc1178246826c
Elkin Angulo <elkinsamir643@gmail.com>
2025-02-22 22:42:59 -0500

    Problema de bootstrap solucionado

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
-        silenceDeprecations: ['legacy-js-api'],
-        quietDeps: true,
-      }
-    }
-  }
+  plugins: [react()]
 })
